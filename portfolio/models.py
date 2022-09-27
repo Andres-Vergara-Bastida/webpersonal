b74a4bc55d02244cdd887030ba0acb6112eac39d
Think04 <andresvergarabastida@gmail.com>
2022-09-27 14:23:06 +0200

    'añadiendo ckeditor'

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name = "titulo")
-    description = models.TextField(verbose_name = "descripción")
+    description = RichTextField(verbose_name='descripción')
     image = models.ImageField(verbose_name = "imagen", upload_to="projects")
     link = models.URLField(null=True, blank=True, verbose_name = "GitHub")
     created = models.DateTimeField(auto_now_add=True, verbose_name = "fecha de creación")
--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'ckeditor',
     'portfolio.apps.PortfolioConfig',
 ]
 
@@ -128,3 +129,16 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Ckeditor
+
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Custom',
+        'toolbar_Custom': [
+            ['Bold', 'Italic', 'Underline'],
+            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+            ['Link', 'Unlink'],
+        ]
+    }
+}
